47ca4da057c5b0f11c031ea1c16bd87f18e8b871
capstonehardware <43168221+capstonehardware@users.noreply.github.com>
2021-04-11 15:36:45 +0530

    First changes
    Capstone project

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from reco_engine import myfuc_PR
+
+app = Flask(__name__)
+
+def func(text1):
+
+    temp = myfuc_PR(text1)
+    print(temp)
+    return (temp)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+@app.route('/join', methods = ['GET', 'POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    word = request.args.get('text1')
+    res = func(text1)
+
+    res_1 = {"output": res}
+    res_1 = {str(key):value for key, value in res_1.items()}
+    return jsonify(result=res_1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
